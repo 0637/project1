ce2e7850664b98cf44e426108cee972e181c9a4f
keerthisree <keerthisree1515@gmail.com>
2020-03-12 15:40:27 +0530

    Updates

---
+++ precache-manifest.7a1c35e436b64ec94ed809af9efdc04f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0b5a2cfb3112e602efb1067c6019ad",
+    "url": "/project1/index.html"
+  },
+  {
+    "revision": "a7301c47eee0e79f4b61",
+    "url": "/project1/static/css/main.4bdcff40.chunk.css"
+  },
+  {
+    "revision": "6357e287429b5cb2f37a",
+    "url": "/project1/static/js/2.8411d881.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/project1/static/js/2.8411d881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7301c47eee0e79f4b61",
+    "url": "/project1/static/js/main.760c9ba9.chunk.js"
+  },
+  {
+    "revision": "fe46e28af8f8da747adb",
+    "url": "/project1/static/js/runtime-main.25af23f5.js"
+  },
+  {
+    "revision": "60038394a7fc4ee7ac46477accc856cb",
+    "url": "/project1/static/media/woman.60038394.svg"
+  }
+]);
